eacf9ef7c0216cf9ac11377ccd5fcf9e1b64f45b
jailcomfranssa <jail_son_0@hotmail.com>
2024-08-18 18:12:15 -0300

    dark mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import Navbar from "./components/Navbar/Navbar"
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  return <div>
-    <Navbar/>
-  </div>
-}
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
-export default App
+  useEffect(() => {
+    const element = document.documentElement;
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  return (
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,48 +1,73 @@
+import PropTypes from "prop-types";
+import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
+
 const NavLinks = [
-    {
-        id: "1",
-        name: "Inicio",
-        link: "/#"
-    },
-    {
-        id: "2",
-        name: "Motos",
-        link: "/#motos"
-    },
-    {
-        id: "3",
-        name: "Sobre",
-        link: "/#sobre"
-    },
-    {
-        id: "4",
-        name: "Booking",
-        link: "/#booking"
-    },
-]
+  {
+    id: "1",
+    name: "Inicio",
+    link: "/#",
+  },
+  {
+    id: "2",
+    name: "Motos",
+    link: "/#motos",
+  },
+  {
+    id: "3",
+    name: "Sobre",
+    link: "/#sobre",
+  },
+  {
+    id: "4",
+    name: "Booking",
+    link: "/#booking",
+  },
+];
 
-const Navbar = () => {
-  return <nav className="shadow-md">
-    <div className="container">
+const Navbar = ({ theme, setTheme }) => {
+  return (
+    <nav className="shadow-md bg-white dark:bg-dark dark:text-white duration-200">
+      <div className="container">
         <div className="flex justify-between items-center">
-            <div>
-                <h1 className="text-3xl font-bold font-serif">Moto Rental</h1>
-            </div>
-            <div className="hidden md:block">
-                <ul className="flex items-center gap-8">
-                    {
-                        NavLinks.map((data) =>(
-                            <li key={data.id} className="py-4">
-                                <a className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium" href={data.link}> {data.name}</a>
-                            </li>
-                        ))
-                    }
-                    
-                </ul>
-            </div>
+          <div>
+            <h1 className="text-3xl font-bold font-serif">Moto Rental</h1>
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {NavLinks.map((data) => (
+                <li key={data.id} className="py-4">
+                  <a
+                    className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium"
+                    href={data.link}
+                  >
+                    {" "}
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            {theme === "dark" ? (
+              <BiSolidSun
+                onClick={() => setTheme("light")}
+                className="text-2xl"
+              />
+            ) : (
+              <BiSolidMoon
+                onClick={() => setTheme("dark")}
+                className="text-2xl"
+              />
+            )}
+          </div>
         </div>
-    </div>
-  </nav>
-}
+      </div>
+    </nav>
+  );
+};
+Navbar.propTypes = {
+  theme: PropTypes.string.isRequired,
+  setTheme: PropTypes.func.isRequired,
+};
 
-export default Navbar
+export default Navbar;
