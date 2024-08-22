427f284eada9a30a090adddac8a4828aa9912e8f
jailcomfranssa <jail_son_0@hotmail.com>
2024-08-22 16:12:06 -0300

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import About from "./components/About/About";
 
 const App = () => {
   const [theme, setTheme] = React.useState(
@@ -33,6 +34,7 @@ const App = () => {
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme}/>
+      <About theme={theme}/>
     </div>
   );
 };
